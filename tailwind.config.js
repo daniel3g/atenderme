cf2e141ecc10ef6d6a765d9718377e8f0ea8e734
Daniel Silva <silvadaniel2006@yahoo.com.br>
2025-06-18 22:00:20 -0300

    Cria estrutura inicial do layout

---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/app/**/*.{js,ts,jsx,tsx}",
+    "./src/pages/**/*.{js,ts,jsx,tsx}",
+    "./src/components/**/*.{js,ts,jsx,tsx}"
+  ],
+  theme: {
+    extend: {
+      colors: {
+        customPrimary: "#1E2A36",
+        customSecondary: "#34BE97",
+        customAccent: "#30B08C",
+        customMuted: "#30B08C",
+        background: "#F8FAFC",
+      },
+    },
+  },
+  plugins: [],
+}
